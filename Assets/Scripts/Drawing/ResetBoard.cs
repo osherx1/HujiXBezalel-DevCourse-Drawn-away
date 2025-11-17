730acd3c33e5e80dd6137e0a899acec3f20a864e
Itai Muntner <itai.muntner@mail.huji.ac.il>
2025-11-17 21:07:54 +0200

    Created a reset button

---
+++ Assets/Scripts/Drawing/ResetBoard.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Drawing
+{
+    public class ResetBoard : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private GameObject lineroot;
+
+        private void OnEnable()
+        {
+            if (!button) return;
+            button.onClick.AddListener(OnResetButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            if (!button) return;
+            button.onClick.RemoveListener(OnResetButtonClicked);
+        }
+
+        private void OnResetButtonClicked()
+        {
+            foreach (Transform child in lineroot.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
