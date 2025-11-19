134404b35325132c6144f3e667d51efdf22b06d5
osherx1 <oshermab@gmail.com>
2025-11-19 19:11:29 +0200

    fix eraser to delete on press

--- Assets/Scripts/Drawing/LineControl/DeleteLine.cs
+++ Assets/Scripts/Drawing/LineControl/DeleteLine.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
 
 namespace Drawing.LineControl
 {
     public class DeleteLine : MonoBehaviour
     {
+        private bool IsErasePressed()
+        {
+    #if ENABLE_INPUT_SYSTEM
+            if (Pen.current != null && Pen.current.tip.isPressed) return true;
+            if (Mouse.current != null && Mouse.current.leftButton.isPressed) return true;
+    #endif
+    #if ENABLE_LEGACY_INPUT_MANAGER
+            if (Input.GetMouseButton(0)) return true;
+    #endif
+            return false;
+        }
+
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.CompareTag("Line"))
-            {
-                Destroy(other.gameObject);
-            }
+            if (!IsErasePressed()) return;
+            if (!other.gameObject.CompareTag("Line")) return;
+            Destroy(other.gameObject);
         }
     }
 }
