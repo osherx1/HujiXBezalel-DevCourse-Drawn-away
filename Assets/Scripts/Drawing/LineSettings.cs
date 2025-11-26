06020d85c598b6cc0642f9b18c0a5eab9551268d
David Nadelsticher <david.nadelsticher@mail.huji.ac.il>
2025-11-26 15:33:27 +0200

    Create a shader with noise-based motion and a shader for iron, add volume control in the Line Setting script

--- Assets/Scripts/Drawing/LineSettings.cs
+++ Assets/Scripts/Drawing/LineSettings.cs
@@ -16,49 +16,62 @@ namespace Drawing
         public GameObject linePrefab; // Added to avoid errors in LineDrawer when using prefab mode.
 
         // ----------------- TAB: APPEARANCE -----------------
-        [ShowIfNot(nameof(usePrefab))]
+        //[ShowIfNot(nameof(usePrefab))]
         [Tab("Appearance")]
         [HorizontalLine("Render Mode")]
         [Tooltip("The color gradient of the line over its lifetime.")]
         public Gradient lineColor;
 
-        [ShowIfNot(nameof(usePrefab))] [Tab("Appearance")] [Range(0.1f, 2f)]
+        //[ShowIfNot(nameof(usePrefab))]
+        [Tab("Appearance")] [Range(0.1f, 2f)]
         // Kept Unity's Range, could use [DynamicSlider] if you want adjustable limits
         public float lineWidth = 0.2f;
 
 
-        [ShowIfNot(nameof(usePrefab))]
+        //[ShowIfNot(nameof(usePrefab))]
         [Tab("Appearance")]
         [ForceFill(errorMessage = "Line Material is required!")] // Alert if empty
         [AssetsOnly]
         // Ensures you don't accidentally drag a scene material
         public Material material;
 
-        [ShowIfNot(nameof(usePrefab))]
+        //[ShowIfNot(nameof(usePrefab))]
         [Tab("Appearance")]
         [HorizontalLine("Vertex Settings", 1, FixedColor.Gray)] // Visual separator
         [Range(0, 90)]
         public int endCapVertices = 0;
 
-        [ShowIfNot(nameof(usePrefab))] [Tab("Appearance")] [Range(0, 90)]
+        //[ShowIfNot(nameof(usePrefab))] 
+        [Tab("Appearance")] [Range(0, 90)]
         public int cornerVertices = 0;
+        //[ShowIfNot(nameof(usePrefab))] 
+        [Tab("Appearance")]
+        //change the line rendering mode
+        [Tooltip("The texture mode of the line.")]
+        //public TextureWrapMode textureMode = TextureWrapMode.Repeat;
+        public LineTextureMode lineTextureMode = LineTextureMode.Stretch;
+       
+        /*public Texture textureMode = TextureMode.Stretch;*/
 
 
         // ----------------- TAB: PHYSICS -----------------
-        [ShowIfNot(nameof(usePrefab))]
+        //[ShowIfNot(nameof(usePrefab))]
         [Tab("Physics")]
         [MessageBox("Disable physics to improve performance on static lines.", MessageBoxType.Info)]
         public bool usePhysics = true;
 
         // The following fields only show if 'usePhysics' is TRUE
-        [ShowIfNot(nameof(usePrefab))] [Tab("Physics")] [ShowIf(nameof(usePhysics))] [Indent(1)][AssetsOnly]
+        //[ShowIfNot(nameof(usePrefab))]
+        [Tab("Physics")] [ShowIf(nameof(usePhysics))] [Indent(1)][AssetsOnly]
         // Indent to show hierarchy visually
         public PhysicsMaterial2D physicsMaterial;
 
-        [ShowIfNot(nameof(usePrefab))] [Tab("Physics")] [ShowIf(nameof(usePhysics))] [Indent(1)]
+        //[ShowIfNot(nameof(usePrefab))]
+        [Tab("Physics")] [ShowIf(nameof(usePhysics))] [Indent(1)]
         public float gravityScaleOverride = 1f;
 
-        [ShowIfNot(nameof(usePrefab))] [Tab("Physics")] [ShowIf(nameof(usePhysics))] [Indent(1)]
+        //[ShowIfNot(nameof(usePrefab))]
+        [Tab("Physics")] [ShowIf(nameof(usePhysics))] [Indent(1)]
         public float massMult = 1f;
 
 
@@ -67,35 +80,45 @@ namespace Drawing
 
         // 1. Draw Sound
         [Hook(nameof(OnDrawSoundBoolChanged))]
-        [ShowIfNot(nameof(usePrefab))]
+        //[ShowIfNot(nameof(usePrefab))]
         [Tab("Sound")]
         [HorizontalLine("Draw Audio")]
         public bool useDrawSound;
 
-        [ShowIfNot(nameof(usePrefab))] [Tab("Sound")] [ShowIf(nameof(useDrawSound))] [Indent(1)]
+        //[ShowIfNot(nameof(usePrefab))]
+        [Tab("Sound")] [ShowIf(nameof(useDrawSound))] [Indent(1)]
         public GameSoundsSo.AudioType drawSound = GameSoundsSo.AudioType.None;
 
 
         // 2. Collision Sound
         [Hook(nameof(OnCollisionSoundBoolChanged))]
-        [ShowIfNot(nameof(usePrefab))]
+        //[ShowIfNot(nameof(usePrefab))]
         [Tab("Sound")]
         [HorizontalLine("Collision Audio")]
         public bool useCollisionSound;
 
-        [ShowIfNot(nameof(usePrefab))] [Tab("Sound")] [ShowIf(nameof(useCollisionSound))] [Indent(1)]
+        //[ShowIfNot(nameof(usePrefab))] 
+        [Tab("Sound")] [ShowIf(nameof(useCollisionSound))] [Indent(1)]
         public GameSoundsSo.AudioType collisionSound = GameSoundsSo.AudioType.None;
 
 
         // 3. Release Sound
         [Hook(nameof(OnReleaseSoundBoolChanged))]
-        [ShowIfNot(nameof(usePrefab))]
+        //[ShowIfNot(nameof(usePrefab))]
         [Tab("Sound")]
         [HorizontalLine("Release Audio")]
         public bool useReleaseSound;
 
-        [ShowIfNot(nameof(usePrefab))] [Tab("Sound")] [ShowIf(nameof(useReleaseSound))] [Indent(1)]
+        //[ShowIfNot(nameof(usePrefab))]
+        [Tab("Sound")] [ShowIf(nameof(useReleaseSound))] [Indent(1)]
         public GameSoundsSo.AudioType releaseSound = GameSoundsSo.AudioType.None;
+        
+        [Tab("Sound")]
+        //[ShowIfNot(nameof(usePrefab))]  [Indent(0)]
+        public float baseVolume = 1.0f;
+        [Tab("Sound")]
+        //[ShowIfNot(nameof(usePrefab))]  [Indent(0)]
+        public bool useCameraShake = false;
 
         public void SetLineSetting(LineSettings otherSettings)
         {
@@ -153,6 +176,10 @@ namespace Drawing
             // Sound - Release
             useReleaseSound = otherSettings.useReleaseSound;
             releaseSound = useReleaseSound ? otherSettings.releaseSound : GameSoundsSo.AudioType.None;
+
+            baseVolume = otherSettings.baseVolume;
+            useCameraShake = otherSettings.useCameraShake;
+            
         }
 
         private void SetPhysics(LineSettings otherSettings)
@@ -177,6 +204,7 @@ namespace Drawing
                 lineColor.SetKeys(otherSettings.lineColor.colorKeys, otherSettings.lineColor.alphaKeys);
                 lineColor.mode = otherSettings.lineColor.mode;
             }
+            lineTextureMode = otherSettings.lineTextureMode;
         }
     }
 }
